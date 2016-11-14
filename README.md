293bb0018173a01b75630005dd53f8e45aff7961
Zhao Xin <pythonchallenge@qq.com>
2016-11-14 22:15:07 +0800

    day2 a moving block

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 - **Day 1** 窗口和方块 A WINDOW AND A BLOCK
 [[源码 source](https://github.com/archtaurus/pysnake/tree/day1)],
 [[视频教程 video tutorial](http://v.youku.com/v_show/id_XMTYzNzQ5MTgxNg==.html)]
+- **Day 2** 移动的方块 A MOVING BLOCK
+[[源码 source](https://github.com/archtaurus/pysnake/tree/day2)],
+[[视频教程 video tutorial](http://v.youku.com/v_show/id_XMTYzOTczMjc2OA==.html)]
--- pysnake.py
+++ pysnake.py
@@ -6,7 +6,7 @@
 # 功能: 主程序文件
 # 许可: General Public License
 # 作者: Zhao Xin (赵鑫) <pythonchallenge@qq.com>
-# 时间: 2016.07.10
+# 时间: 2016.07.12
 
 import os
 import sys
@@ -26,13 +26,41 @@ game_bgcolor = 33, 66, 33
 square_color = 33, 255, 33
 square_x, square_y = 0, 0
 square_size = 20
+square_speed = 2
+square_speed_x, square_speed_y = square_speed, 0
 
 while game_playing:
     # 用户控制
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_playing = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                square_speed_x = 0
+                square_speed_y = -square_speed
+            elif event.key == pygame.K_DOWN:
+                square_speed_x = 0
+                square_speed_y = square_speed
+            elif event.key == pygame.K_LEFT:
+                square_speed_x = -square_speed
+                square_speed_y = 0
+            elif event.key == pygame.K_RIGHT:
+                square_speed_x = square_speed
+                square_speed_y = 0
     # 更新数据
+    square_x += square_speed_x
+    square_y += square_speed_y
+    if square_x < 0:
+        square_x = 0
+    elif square_x > game_screen_width - square_size:
+        square_x = game_screen_width - square_size
+    if square_y < 0:
+        square_y = 0
+    elif square_y > game_screen_height - square_size:
+        square_y = game_screen_height - square_size
+    print "坐标：x %3d, y %3d, 速度：x %d, y %d" % (square_x, square_y,
+                                              square_speed_x,
+                                              square_speed_y)
     # 更新画面
     game_screen.fill(game_bgcolor)
     pygame.draw.rect(game_screen, square_color,
